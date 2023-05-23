20bcc27695457d6bd8b1d166fbcf4e0afe11a756
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-05-23 19:20:08 +0800

    add mq.sh

---
+++ mq.sh
@@ -0,0 +1,5 @@
+docker run --hostname=rabbitmq --name rabbitmq \
+    --env=RABBITMQ_DEFAULT_PASS=9660527 \
+    --env=RABBITMQ_DEFAULT_USER=root  \
+    --volume=mq-data:/var/lib/rabbitmq  \
+    -p 15672:15672 -p 5672:5672  -d rabbitmq:management
