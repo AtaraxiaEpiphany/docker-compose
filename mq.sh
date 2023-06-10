8f992d1515fbfebab559c1b4f2ba33ccd0c0b223
Hannibal <yuewu0527@qq..com>
2023-06-10 16:34:20 +0800

    update mq.sh

--- mq.sh
+++ mq.sh
@@ -1,5 +1,6 @@
 docker run --hostname=rabbitmq --name rabbitmq \
     --env=RABBITMQ_DEFAULT_PASS=9660527 \
     --env=RABBITMQ_DEFAULT_USER=root  \
-    --volume=mq-data:/etc/rabbitmq  \
+    --volume=mq-data:/var/lib/rabbitmq \
+    --volume=mq-conf:/etc/rabbitmq  \
     -p 15672:15672 -p 5672:5672  -d rabbitmq:management
