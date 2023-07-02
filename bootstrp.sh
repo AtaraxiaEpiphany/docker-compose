5a060eee84a98c2eac01831628cfce7ffce604bd
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-07-02 14:05:01 +0800

    Update init.sh

--- bootstrp.sh
+++ bootstrp.sh
@@ -1,8 +1,8 @@
 docker-compose down -v\
-&& docker volume rm e-data k-data certs e-plugins; \
-docker volume create e-data \
-&& docker volume create k-data \
+&& docker volume rm es-data kibana-data certs es-plugins; \
+docker volume create es-data \
+&& docker volume create kibana-data \
 && docker volume create certs \
-&& docker volume create e-plugins \
+&& docker volume create es-plugins \
 && docker-compose  up -d
 
